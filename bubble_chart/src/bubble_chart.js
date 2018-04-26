d7fdb97ec6b61f6b108f9a318fbc2db111f02aac
bobrandt <31392557+bobrandt@users.noreply.github.com>
2018-04-26 09:16:42 -0700

    adding bubble chart code

---
+++ bubble_chart/src/bubble_chart.js
@@ -0,0 +1,332 @@
+
+
+/* bubbleChart creation function. Returns a function that will
+ * instantiate a new bubble chart given a DOM element to display
+ * it in and a dataset to visualize.
+ *
+ * Organization and style inspired by:
+ * https://bost.ocks.org/mike/chart/
+ *
+ */
+function bubbleChart() {
+  // Constants for sizing
+  var width = 940;
+  var height = 600;
+
+  // tooltip for mouseover functionality
+  var tooltip = floatingTooltip('gates_tooltip', 240);
+
+  // Locations to move bubbles towards, depending
+  // on which view mode is selected.
+  var center = { x: width / 2, y: height / 2 };
+
+  // Used when setting up force and
+  // moving around nodes
+  var damper = 0.102;
+
+  // These will be set in create_nodes and create_vis
+  var svg = null;
+  var bubbles = null;
+  var nodes = [];
+
+  // Charge function that is called for each node.
+  // Charge is proportional to the diameter of the
+  // circle (which is stored in the radius attribute
+  // of the circle's associated data.
+  // This is done to allow for accurate collision
+  // detection with nodes of different sizes.
+  // Charge is negative because we want nodes to repel.
+  // Dividing by 8 scales down the charge to be
+  // appropriate for the visualization dimensions.
+  function charge(d) {
+    return -Math.pow(d.radius, 2.0) / 8;
+  }
+
+  // Here we create a force layout and
+  // configure it to use the charge function
+  // from above. This also sets some contants
+  // to specify how the force layout should behave.
+  // More configuration is done below.
+  var force = d3.layout.force()
+    .size([width, height])
+    .charge(charge)
+    .gravity(-0.01)
+    .friction(0.9);
+
+
+  // Nice looking colors - no reason to buck the trend
+  var fillColor = d3.scale.ordinal()
+    .domain(['low', 'medium', 'high'])
+    .range(['#d84b2a', '#beccae', '#7aa25c']);
+
+  // Sizes bubbles based on their area instead of raw radius
+  var radiusScale = d3.scale.pow()
+    .exponent(0.5)
+    .range([2, 50]);
+
+  /*
+   * This data manipulation function takes the raw data from
+   * the CSV file and converts it into an array of node objects.
+   * Each node will store data and visualization values to visualize
+   * a bubble.
+   *
+   * rawData is expected to be an array of data objects, read in from
+   * one of d3's loading functions like d3.csv.
+   *
+   * This function returns the new node array, with a node in that
+   * array for each element in the rawData input.
+   */
+  function createNodes(rawData) {
+    // Use map() to convert raw data into node data.
+    // Checkout http://learnjsdata.com/ for more on
+    // working with data.
+    var myNodes = rawData.map(function (d) {
+      return {
+        id: d.Id,
+        radius: radiusScale(+d.Headcount),
+        value: d.Headcount,
+        name: d.Company,
+        org: d.AcquiredBy,
+        group: d.BroadCategory,
+        year: d.Founded,
+        x: Math.random() * 900,
+        y: Math.random() * 800
+      };
+    });
+
+    // sort them to prevent occlusion of smaller nodes.
+    myNodes.sort(function (a, b) { return b.value - a.value; });
+
+    return myNodes;
+  }
+
+  /*
+   * Main entry point to the bubble chart. This function is returned
+   * by the parent closure. It prepares the rawData for visualization
+   * and adds an svg element to the provided selector and starts the
+   * visualization creation process.
+   *
+   * selector is expected to be a DOM element or CSS selector that
+   * points to the parent element of the bubble chart. Inside this
+   * element, the code will add the SVG continer for the visualization.
+   *
+   * rawData is expected to be an array of data objects as provided by
+   * a d3 loading function like d3.csv.
+   */
+  var chart = function chart(selector, rawData) {
+    // Use the max total_amount in the data as the max in the scale's domain
+    // note we have to ensure the total_amount is a number by converting it
+    // with `+`.
+
+    var new_dict = {};
+
+    var current_year = 2000;
+    var end_year = 2018;
+    // FUNCTION TO RECONSTRUCT DATA
+    for (current_year; current_year <= end_year; current_year++) { 
+
+      var new_data = []
+      rawData.forEach(function(d) {
+          // DETERMINES WHICH DATA SHOULD BE INCLUDED
+          if (d['Founded'] <= current_year && d['Acquisition date'] > current_year) {
+              new_data.push(d)
+          }
+      });
+      new_dict[current_year] = new_data
+    }
+
+    rawData = new_dict[2011];
+
+
+    var maxAmount = d3.max(rawData, function (d) { return +d.Headcount; });
+    radiusScale.domain([0, maxAmount]);
+
+    nodes = createNodes(rawData);
+    // Set the force's nodes to our newly created nodes array.
+    force.nodes(nodes);
+
+    // Create a SVG element inside the provided selector
+    // with desired size.
+    svg = d3.select(selector)
+      .append('svg')
+      .attr('width', width)
+      .attr('height', height);
+
+    // Bind nodes data to what will become DOM elements to represent them.
+    bubbles = svg.selectAll('.bubble')
+      .data(nodes, function (d) { return d.id; });
+
+    // Create new circle elements each with class `bubble`.
+    // There will be one circle.bubble for each object in the nodes array.
+    // Initially, their radius (r attribute) will be 0.
+    bubbles.enter().append('circle')
+      .classed('bubble', true)
+      .attr('r', 0)
+      .attr('fill', function (d) { return fillColor(d.group); })
+      .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+      .attr('stroke-width', 2)
+      .on('mouseover', showDetail)
+      .on('mouseout', hideDetail);
+
+    // Fancy transition to make bubbles appear, ending with the
+    // correct radius
+    bubbles.transition()
+      .duration(2000)
+      .attr('r', function (d) { return d.radius; });
+
+    // Set initial layout to single group.
+    groupBubbles();
+
+    // HERE IS THE CLICK EVENT !!!!!!!!!!
+    d3.select('#next').on('click', function () {
+      nodes = createNodes(new_dict[2012]);
+      // Set the force's nodes to our newly created nodes array.
+      force.nodes(nodes);
+
+      bubbles = svg.selectAll('.bubble')
+      .data(nodes, function (d) { 
+        return d.id; });
+
+      // Create new circle elements each with class `bubble`.
+      // There will be one circle.bubble for each object in the nodes array.
+      // Initially, their radius (r attribute) will be 0.
+      bubbles.enter().append('circle')
+        .classed('bubble', true)
+        .attr('r', 5)
+        .attr('fill', function (d) { return fillColor(d.group); })
+        .attr('stroke', function (d) { return d3.rgb(fillColor(d.group)).darker(); })
+        .attr('stroke-width', 2)
+        .on('mouseover', showDetail)
+        .on('mouseout', hideDetail)
+
+
+      // Fancy transition to make bubbles appear, ending with the
+      // correct radius
+      bubbles.transition()
+        .duration(100000)
+        .attr('r', function (d) { return d.radius; });
+
+      // Set initial layout to single group.
+      groupBubbles();
+
+
+    });
+
+
+  };
+
+  /*
+   * Sets visualization in "single group mode".
+   * The year labels are hidden and the force layout
+   * tick function is set to move all nodes to the
+   * center of the visualization.
+   */
+  function groupBubbles() {
+    // hideYears();
+
+    force.on('tick', function (e) {
+      bubbles.each(moveToCenter(e.alpha))
+        .attr('cx', function (d) { return d.x; })
+        .attr('cy', function (d) { return d.y; });
+    });
+
+    force.start();
+  }
+
+  /*
+   * Helper function for "single group mode".
+   * Returns a function that takes the data for a
+   * single node and adjusts the position values
+   * of that node to move it toward the center of
+   * the visualization.
+   *
+   * Positioning is adjusted by the force layout's
+   * alpha parameter which gets smaller and smaller as
+   * the force layout runs. This makes the impact of
+   * this moving get reduced as each node gets closer to
+   * its destination, and so allows other forces like the
+   * node's charge force to also impact final location.
+   */
+  function moveToCenter(alpha) {
+    return function (d) {
+      d.x = d.x + (center.x - d.x) * damper * alpha;
+      d.y = d.y + (center.y - d.y) * damper * alpha;
+    };
+  }
+
+
+  /*
+   * Function called on mouseover to display the
+   * details of a bubble in the tooltip.
+   */
+  function showDetail(d) {
+    // change outline to indicate hover state.
+    d3.select(this).attr('stroke', 'black');
+
+    var content = '<span class="name">Company: </span><span class="value">' +
+                  d.name +
+                  '</span><br/>' +
+                  '<span class="name">Employees: </span><span class="value">' +
+                  addCommas(d.value) +
+                  '+</span><br/>' +
+                  '<span class="name">Founded: </span><span class="value">' +
+                  d.year +
+                  '</span>';
+    tooltip.showTooltip(content, d3.event);
+  }
+
+  /*
+   * Hides tooltip
+   */
+  function hideDetail(d) {
+    // reset outline
+    d3.select(this)
+      .attr('stroke', d3.rgb(fillColor(d.group)).darker());
+
+    tooltip.hideTooltip();
+  }
+
+  // return the chart function from closure.
+  return chart;
+}
+
+/*
+ * Below is the initialization code as well as some helper functions
+ * to create a new bubble chart instance, load the data, and display it.
+ */
+
+var myBubbleChart = bubbleChart();
+
+/*
+ * Function called once data is loaded from CSV.
+ * Calls bubble chart function to display inside #vis div.
+ */
+function display(error, data) {
+  if (error) {
+    console.log(error);
+  }
+    myBubbleChart('#vis', data);
+}
+
+/*
+ * Helper function to convert a number into a string
+ * and add commas to it to improve presentation.
+ */
+function addCommas(nStr) {
+  nStr += '';
+  var x = nStr.split('.');
+  var x1 = x[0];
+  var x2 = x.length > 1 ? '.' + x[1] : '';
+  var rgx = /(\d+)(\d{3})/;
+  while (rgx.test(x1)) {
+    x1 = x1.replace(rgx, '$1' + ',' + '$2');
+  }
+
+  return x1 + x2;
+}
+
+// Load the data.
+d3.csv('data/AcquisitionDatabaseFinalFixed_bo.csv', display);
+
+// setup the buttons.
+// setupButtons();
